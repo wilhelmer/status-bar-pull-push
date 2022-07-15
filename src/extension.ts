4d8c953ed33862b1dbdc6eeffca82796af2a7e9d
Lars Wilhelmer <lars.wilhelmer@baslerweb.com>
2022-07-15 16:27:44 +0200

    Immediately update buttons on commit

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,19 @@
 import * as vscode from 'vscode';
 import { execSync } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
 
 let pushItem: vscode.StatusBarItem;
 let pullItem: vscode.StatusBarItem;
+let workspaceDir = "";
+let fsWait = false;
 
 export function activate({ subscriptions }: vscode.ExtensionContext) {
+    if (!vscode.workspace.workspaceFolders) return;
+    
+    // Get workspace directory
+    // We use substring(1) to cut the leading slash for Windows compatibility
+    workspaceDir = vscode.workspace.workspaceFolders[0].uri.path.substring(1);
 
     // Create status bar items
     pullItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
@@ -19,10 +28,10 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
     subscriptions.push(pullItem);
     subscriptions.push(pushItem);
 
-    // Update buttons every X seconds
+    // Update buttons every 5 seconds
     const o = (): vscode.Disposable => {
         const id = setInterval(() => {
-            updateStatusBarItem();
+            updateStatusBarItems();
         }, 5000);
         return {
             dispose() {
@@ -31,12 +40,25 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
         };
     };
     subscriptions.push(o());
-    
+
+    // Update buttons on commit
+    try {
+        const gitCommit = path.join(workspaceDir, ".git", "COMMIT_EDITMSG");
+        fs.watch(gitCommit, function (evt, filename) {
+            if (filename) {
+                if (debounceFsWatch()) return;
+                updateStatusBarItems();
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
     // Update on load
-    updateStatusBarItem();
+    updateStatusBarItems();
 }
 
-function updateStatusBarItem(): void {
+function updateStatusBarItems(): void {
     const countPull = getGitCommitsCount(true);
     const countPush = getGitCommitsCount(false);
 
@@ -49,23 +71,27 @@ function updateStatusBarItem(): void {
     else {
         pullItem.text = `${countPull}\u2009$(arrow-down)`;
         pushItem.text = `${countPush}\u2009$(arrow-up)`;
-    
+
         pullItem.show();
         pushItem.show();
     }
 }
 
-function getGitCommitsCount(pull = true) {
-    if (!vscode.workspace.workspaceFolders) return;
-
+function getGitCommitsCount(pull: boolean) {
     try {
         // Count commits not yet pulled from or pushed to the remote repository
         const command = pull ? `git rev-list --count HEAD..@{u}` : `git rev-list --count @{u}..HEAD`;
-        const workspaceDir = vscode.workspace.workspaceFolders[0].uri.path.substring(1);
         return execSync(command, {
             cwd: workspaceDir,
         }).toString().trim();
     } catch (error) {
         console.log(error);
     }
+}
+
+function debounceFsWatch() {
+    if (fsWait) return true;
+    setTimeout(() => {
+        fsWait = false;
+    }, 100);
 }
